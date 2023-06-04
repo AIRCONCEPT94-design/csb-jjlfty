7a996f36e028b29008fa15899a4f8c66a186bf55
Code Sandbox <hello@codesandbox.io>
2023-06-04 09:50:09 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.df8986fce77d34f8d8257d9a7ad5dd52.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a0e2c190e45241c900c700cfe537c2c",
+    "url": "https://airconcept94-design.github.io/csb-jjlfty/index.html"
+  },
+  {
+    "revision": "eb44d9bd351ef69b9c45",
+    "url": "https://airconcept94-design.github.io/csb-jjlfty/static/js/2.2b04f5e7.chunk.js"
+  },
+  {
+    "revision": "f0fbbfa0c7744fd9d2b2",
+    "url": "https://airconcept94-design.github.io/csb-jjlfty/static/js/main.6b6417b4.chunk.js"
+  },
+  {
+    "revision": "d981a1dd61a47816fc7b",
+    "url": "https://airconcept94-design.github.io/csb-jjlfty/static/js/runtime-main.daaeea76.js"
+  }
+]);
